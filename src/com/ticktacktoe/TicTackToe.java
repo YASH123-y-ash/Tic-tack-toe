3e7496bfddd2c4f45c1c1e87fad123fcc74ef100
Yash-y-ash <wardhan696@gmail.com>
2021-03-27 10:26:07 +0530

    Added:allow-player-to-choose-X-O

--- src/com/ticktacktoe/TicTackToe.java
+++ src/com/ticktacktoe/TicTackToe.java
@@ -1,7 +1,21 @@
 package com.ticktacktoe;
-
+import java.util.*;
 public class TicTackToe {
 
+	static char chooseXorO()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("select X or o");
+		char ch1 = sc.next().charAt(0);
+		
+		while(ch1 != 'x' && ch1 != 'o')
+		{
+			System.out.println("select X or o");
+			ch1 = sc.next().charAt(0);
+		}
+		return ch1;
+	}
+	
 	public static void main(String[] args) {
 	
 		System.out.println("Tic tac toe");
@@ -10,6 +24,7 @@ public class TicTackToe {
 		{
 			gameBoard[i] = ' ';
 		}
+		char input = chooseXorO();
 	}
 }
 
