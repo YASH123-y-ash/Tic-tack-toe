8fd6d49eba9895a4fe9670b598c52aeec9575343
Yash-y-ash <wardhan696@gmail.com>
2021-03-27 09:22:27 +0530

    Added:creating tic tack toe game board

---
+++ src/com/ticktacktoe/TicTackToe.java
@@ -0,0 +1,15 @@
+package com.ticktacktoe;
+
+public class TicTackToe {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Tic tac toe");
+		char[] gameBoard = new char[10];
+		for(int i = 0 ;i < gameBoard.length ; i++)
+		{
+			gameBoard[i] = ' ';
+		}
+	}
+}
+
