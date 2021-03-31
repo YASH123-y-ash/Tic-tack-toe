1eaeb6feea93a90ab6247f87ae9f3de0546992dc
Yash-y-ash <wardhan696@gmail.com>
2021-03-31 15:51:00 +0530

    Added:to choose who plays first player or computer

--- src/com/ticktacktoe/TicTackToe.java
+++ src/com/ticktacktoe/TicTackToe.java
@@ -156,25 +156,7 @@ public class TicTackToe {
 		else
 			return 0;
 	}
-<<<<<<< HEAD
-	
-	public void desireMove()
-	{
-	System.out.println("--------------");
-	System.out.println("|  " +board[0]+ " | "+board[1] + " | "+board[2]+ " | ");
-	System.out.println("--------------");
-	System.out.println("|  " +board[3]+ " | "+board[4] + " | "+board[5]+ " | ");
-	System.out.println("--------------");
-	System.out.println("|  " +board[6]+ " | "+board[7] + " | "+board[8]+ " | ");
-	System.out.println("--------------");
-	
-	}
-	
-	
-=======
 
-
->>>>>>> UC6-who-play-first
 	public static void main(String[] args) {
 
 		sizeOfBoard();
