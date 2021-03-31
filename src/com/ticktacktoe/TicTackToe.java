55cb446cf446e13df3ea9c7a23b34a7718c6a535
Yash-y-ash <wardhan696@gmail.com>
2021-03-31 15:58:55 +0530

    Added:choose between x or o

--- src/com/ticktacktoe/TicTackToe.java
+++ src/com/ticktacktoe/TicTackToe.java
@@ -1,52 +1,58 @@
 package com.ticktacktoe;
+
 import java.util.*;
 public class TicTackToe {
+	
+	public static char[] board = new char[9];
 
-	static String[] board;
-	static void printBoard()
-    {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | "
-                           + board[1] + " | " + board[2]
-                           + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | "
-                           + board[4] + " | " + board[5]
-                           + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | "
-                           + board[7] + " | " + board[8]
-                           + " |");
-        System.out.println("|---|---|---|");
-    }
+	public static char playerSymbol;
+	public static char computerSymbol;
 
-	static char chooseXorO()
+
+	//for size of board
+	public static void sizeOfBoard()
 	{
+		for(int i=0; i<board.length;i++)
+		{
+			board[i] = ' ';
+		}
+	}
+
+	//choose between x or o
+	public static void chooseXorO()
+	{
+		System.out.println("Choose x or o");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("select X or o");
-		char ch1 = sc.next().charAt(0);
-		
-		while(ch1 != 'x' && ch1 != 'o')
+		char chooseLetter = sc.next().charAt(0);
+		while(chooseLetter != 'x' && chooseLetter != 'o')
 		{
 			System.out.println("select X or o");
-			ch1 = sc.next().charAt(0);
+			chooseLetter = sc.next().charAt(0);
 		}
-		return ch1;
+		if(chooseLetter == 'x')
+		{
+			System.out.println("player symbol to play the game is : "+chooseLetter);
+			System.out.println("computer symbol to play the game is : "+ "o");
+
+			playerSymbol = 'x';
+
+		}
+		else if(chooseLetter == 'o')
+		{
+			System.out.println("computer symbol to play the game is : "+chooseLetter);
+			System.out.println("player symbol to play the game is : "+"x");
+
+			computerSymbol = 'o';
+
+		}
+		else
+			System.out.println("invalid input choose between x or o");
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("welcome to Tic tac toe game");
-				char input = chooseXorO();
-				 board = new String[9];
-			  
-			        String winner = null;
-				for (int a = 0; a < 9; a++) {
-		            board[a] = String.valueOf(a + 1);
-		        }
-		  
-			        System.out.println("Welcome to 3x3 Tic Tac Toe.");
-			        printBoard();
-			  
+	
+		sizeOfBoard();
+		chooseXorO();
 	}
 	
 	  
